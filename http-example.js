50a82b9a339ad5f0ee38b0f2c5cef432e8aa2c64
Albert F. Montoya <albert@montoyamedia.com>
2019-07-15 15:45:17 -0700

    Add files via upload

---
+++ http-example.js
@@ -0,0 +1,11 @@
+//Simple HTTP node server
+//montoyamedia.com
+
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(8080);
